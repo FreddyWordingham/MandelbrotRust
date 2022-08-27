4c888a7293006390d1e12b22aa045035ceada20b
FreddyWordingham <freddy@digilab.com>
2022-08-27 03:27:43 +0100

    Improved example image

--- image.py
+++ image.py
@@ -3,9 +3,9 @@ import mandelbrot
 
 centre = mandelbrot.Complex(-0.4605111, 0.56011)
 max_iter = 1000
-scale = 1.0e-2
+scale = 1.25e-2
 rate = 0.9
-res = [1000, 1000]
+res = [3440, 3440]
 frames = 1
 
 mandelbrot.mandelbrot_video(centre, scale, rate, res, frames, max_iter)
